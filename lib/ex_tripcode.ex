a316325e1cbb4696a77da09bf81ce37108f36b97
krepflap <krepflap@tuta.io>
2019-06-16 16:40:47 +0200

    mix new

---
+++ lib/ex_tripcode.ex
@@ -0,0 +1,18 @@
+defmodule ExTripcode do
+  @moduledoc """
+  Documentation for ExTripcode.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExTripcode.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_tripcode_test.exs
@@ -0,0 +1,8 @@
+defmodule ExTripcodeTest do
+  use ExUnit.Case
+  doctest ExTripcode
+
+  test "greets the world" do
+    assert ExTripcode.hello() == :world
+  end
+end
