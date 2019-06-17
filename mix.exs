330ccd42a36d6a8f2301e62e9f35503a319ea56d
krepflap <krepflap@tuta.io>
2019-06-17 09:20:58 +0200

    Add needed deps

--- mix.exs
+++ mix.exs
@@ -21,8 +21,8 @@ defmodule ExTripcode.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:crypt3, "~> 1.0"},
+      {:iconv, "~> 1.0"},
     ]
   end
 end
