facad59f7755bbb688175d2b92944c33c069a507
krepflap <krepflap@tuta.io>
2019-06-18 17:26:20 +0200

    Refactor tests

--- test/ex_tripcode_test.exs
+++ test/ex_tripcode_test.exs
@@ -2,10 +2,21 @@ defmodule ExTripcodeTest do
   use ExUnit.Case
   doctest ExTripcode
 
+  @seed """
+  FW6I5Es311r2JV6EJSnrR2+hw37jIfGI0FB0XU5+9lua9iCCrwgkZDVRZ+1PuClqC+78FiA6hhh\
+  XU1oq6OyFx/MWYx6tKsYeSA8cAs969NNMQ98SzdLFD7ZifHFreNdrfub3xNQBU21rknftdESFRT\
+  Ur44nqCZ0wyzVVDySGUZkbtyHhnj+cknbZqDu/wjhX/HjSitRbtotpozhF4C9F+MoQCr3LgKg+C\
+  iYHs3Phd3xk6UC2BG2EU83PignJMOCfxzA02gpVHuwy3sx7hX4yvOYBvo0kCsk7B5DURBaNWH0s\
+  rWz4MpXRcDletGGCeKOz9Hn1WXJu78ZdxC58VDl20UIT9er5QLnWiF1giIGQXQMqBB+Rd48/suE\
+  WAOH2H9WYimTJWTrK397HMWepK6LJaUB5GdIk56ZAULjgZB29qx8Cl+1K0JWQ0SI5LrdjgyZZUT\
+  X8LB/6Coix9e6+3c05Pk6Bi1GWsMWcJUf7rL9tpsxROtq0AAQBPQ0rTlstFEziwm3vRaTZvPRbo\
+  QfREta09VA+tRiWfN3XP+1bbMS9exKacGLMxR/bmO5A57AgQF+bPjhif5M/OOJ6J/76q0JDHA==\
+  """
+
   describe "regular tripcodes" do
     test "should generate regular tripcodes" do
-      assert ExTripcode.hash("overcompensate") == "sGqtdX5oJc"
-      assert ExTripcode.hash("pepperoni") == "HohErJmbN6"
+      assert_tripcode("overcompensate", "sGqtdX5oJc")
+      assert_tripcode("pepperoni", "HohErJmbN6")
     end
 
     test "length of regular tripcode should always be 10 chars" do
@@ -13,51 +24,40 @@ defmodule ExTripcodeTest do
     end
 
     test "should work with special chars" do
-      assert ExTripcode.hash("廨A齬ﾙｲb") == "sTrIKeleSs"
-      assert ExTripcode.hash("エリクサー") == "VTaBo/Ew8o"
-      assert ExTripcode.hash("糯ｫT弓(窶") == "Pants.f1Fk"
+      assert_tripcode("廨A齬ﾙｲb", "sTrIKeleSs")
+      assert_tripcode("エリクサー", "VTaBo/Ew8o")
+      assert_tripcode("糯ｫT弓(窶", "Pants.f1Fk")
 
-      assert ExTripcode.hash("$$$$$$$$$$$$$$$$$$") == "dQCqKwAumo"
-      assert ExTripcode.hash("€€€€€€€€€€€€€€€€€€") == "bIffqtgKTg"
+      assert_tripcode("$$$$$$$$$$$$$$$$$$", "dQCqKwAumo")
+      assert_tripcode("€€€€€€€€€€€€€€€€€€", "bIffqtgKTg")
     end
 
     test "should work with html chars" do
-      assert ExTripcode.hash("&elixir") == "Q8CfKxygXk"
+      assert_tripcode("&elixir", "Q8CfKxygXk")
     end
 
     test "should work with short values" do
-      assert ExTripcode.hash("a") == "ZnBI2EKkq."
+      assert_tripcode("a", "ZnBI2EKkq.")
     end
 
     test "should work with long values" do
-      assert ExTripcode.hash("gV4ZhqkrjBka)GiWi(zKJHYPShVN!^TsG%6Xxo9Q") == "3ps/PcSAjk"
+      assert_tripcode("gV4ZhqkrjBka)GiWi(zKJHYPShVN!^TsG%6Xxo9Q", "3ps/PcSAjk")
     end
 
     test "however, we expect long values to be cut off after 8 chars" do
-      assert ExTripcode.hash("gV4Zhqkr") ==
-               ExTripcode.hash("gV4ZhqkrjBka)GiWi(zKJHYPShVN!^TsG%6Xxo9Q")
+      assert_tripcode("gV4Zhqkr", "3ps/PcSAjk")
+      assert_tripcode("gV4Zhqkrj", "3ps/PcSAjk")
     end
 
     test "empty value should give empty result" do
-      assert ExTripcode.hash("") == ""
+      assert_tripcode("", "")
     end
   end
 
   describe "secure tripcodes" do
-    @seed """
-    FW6I5Es311r2JV6EJSnrR2+hw37jIfGI0FB0XU5+9lua9iCCrwgkZDVRZ+1PuClqC+78FiA6hhh\
-    XU1oq6OyFx/MWYx6tKsYeSA8cAs969NNMQ98SzdLFD7ZifHFreNdrfub3xNQBU21rknftdESFRT\
-    Ur44nqCZ0wyzVVDySGUZkbtyHhnj+cknbZqDu/wjhX/HjSitRbtotpozhF4C9F+MoQCr3LgKg+C\
-    iYHs3Phd3xk6UC2BG2EU83PignJMOCfxzA02gpVHuwy3sx7hX4yvOYBvo0kCsk7B5DURBaNWH0s\
-    rWz4MpXRcDletGGCeKOz9Hn1WXJu78ZdxC58VDl20UIT9er5QLnWiF1giIGQXQMqBB+Rd48/suE\
-    WAOH2H9WYimTJWTrK397HMWepK6LJaUB5GdIk56ZAULjgZB29qx8Cl+1K0JWQ0SI5LrdjgyZZUT\
-    X8LB/6Coix9e6+3c05Pk6Bi1GWsMWcJUf7rL9tpsxROtq0AAQBPQ0rTlstFEziwm3vRaTZvPRbo\
-    QfREta09VA+tRiWfN3XP+1bbMS9exKacGLMxR/bmO5A57AgQF+bPjhif5M/OOJ6J/76q0JDHA==\
-    """
-
     test "should generate regular tripcodes with long seed" do
-      assert ExTripcode.hash("overcompensate", @seed) == "A97Af57hncyxk2g"
-      assert ExTripcode.hash("pepperoni", @seed) == "7qM+7vweIG1RyQc"
+      assert_tripcode("overcompensate", @seed, "A97Af57hncyxk2g")
+      assert_tripcode("pepperoni", @seed, "7qM+7vweIG1RyQc")
     end
 
     test "length of secure tripcode should always be 15 chars" do
@@ -66,8 +66,16 @@ defmodule ExTripcodeTest do
     end
 
     test "empty value should give empty result" do
-      assert ExTripcode.hash("", "") == ""
-      assert ExTripcode.hash("", "secret") == ""
+      assert_tripcode("", "", "")
+      assert_tripcode("", "secret", "")
     end
+  end
+
+  defp assert_tripcode(input, expected) do
+    assert ExTripcode.hash(input) == expected
+  end
+
+  defp assert_tripcode(input, seed, expected) do
+    assert ExTripcode.hash(input, seed) == expected
   end
 end
