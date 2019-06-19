8c4ae1fb3fc532f9d6c4c381ea4671984ad2ffdc
krepflap <krepflap@tuta.io>
2019-06-19 13:59:19 +0200

    Add ex_doc

--- mix.exs
+++ mix.exs
@@ -27,7 +27,8 @@ defmodule ExTripcode.MixProject do
     [
       {:crypt3, "~> 1.0"},
       {:iconv, "~> 1.0"},
-      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 
